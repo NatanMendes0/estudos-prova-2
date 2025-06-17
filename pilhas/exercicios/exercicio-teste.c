d147e4748b1e81162f61e628d2d9a218fda6e20f
Natan Mendes <natancmendes@gmail.com>
2025-06-16 22:40:34 -0300

    Feat: Adicionar estruturas basicas de pilhas + Começo dos exercicios

---
+++ pilhas/exercicios/exercicio-teste.c
@@ -0,0 +1,7 @@
+/**
+ * Crie um programa em C que receba uma string (ex: "{[()]}" ou "{[(])}") e
+ * utilize a pilha que você implementou para determinar se os parênteses, colchetes e chaves
+ * estão corretamente balanceados.
+ */
+
+ 
